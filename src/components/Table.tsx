386e15c2b1a4f1e594ee274653cf81158c3b3894
Jess <jessicalaurenwilson1@gmail.com>
2024-04-04 15:00:50 +0100

    Refactor and test passing

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { data } from '../data';
 
 const Table = () => {
-	const [isVisible, setIsVisible] = useState<boolean>(false);
+	const [expandedFolder, setExpandedFolder] = useState<string | null>(null);
 
-	const handleFolders = () => {
-		setIsVisible((prevState) => !prevState);
+	const handleFolders = (folderName: string) => {
+		setExpandedFolder(
+			(prevFolder) => (prevFolder === folderName ? null : folderName) // Toggle the expandedFolder state to either the clicked folder name or null
+		);
 	};
 
 	return (
@@ -15,33 +17,36 @@ const Table = () => {
 					<tr>
 						<th className='py-2 px-3'>All documents</th>
 					</tr>
-					{data.map((document, key) =>
-						document.type === 'folder' ? (
-							<tr
-								key={key}
-								className='px-6 py-4 whitespace-nowrap border-b border-neutral-400 rounded hover:cursor-pointer'
-								onClick={handleFolders}
-							>
-								<td className='px-3 py-2 font-bold'>{document.name}</td>
-								<td className='px-3 py-2'>
-									{isVisible &&
-										document.files?.map((file, fileKey) => (
-											<span key={fileKey} className='px-4'>
-												{file.name}
-											</span>
-										))}
-								</td>
-							</tr>
-						) : (
-							<tr
-								key={key}
-								className='px-6 py-4 whitespace-nowrap border-b border-neutral-400 rounded'
-							>
-								<td className='px-3 py-2 font-bold'>{document.name}</td>
-								<td className='px-3 py-2'></td>
-							</tr>
-						)
-					)}
+					{data.map((document, key) => (
+						<React.Fragment key={key}>
+							{document.type === 'folder' && (
+								<>
+									<tr
+										className='px-6 py-4 whitespace-nowrap border-b border-neutral-400 rounded hover:cursor-pointer'
+										onClick={() => handleFolders(document.name)}
+									>
+										<td className='px-3 py-2 font-bold'>{document.name}</td>
+									</tr>
+									{expandedFolder === document.name && document.files && (
+										<tr>
+											<td>
+												{document.files.map((file, fileKey) => (
+													<p key={fileKey} className='px-6 py-2'>
+														{file.name}
+													</p>
+												))}
+											</td>
+										</tr>
+									)}
+								</>
+							)}
+							{document.type !== 'folder' && (
+								<tr className='px-6 py-4 whitespace-nowrap border-b border-neutral-400 rounded'>
+									<td className='px-3 py-2 font-bold'>{document.name}</td>
+								</tr>
+							)}
+						</React.Fragment>
+					))}
 				</tbody>
 			</table>
 		</section>
