e4532a800764af21622e9c3060c5922e5dc63c04
Jess <jessicalaurenwilson1@gmail.com>
2024-04-04 13:43:24 +0100

    Table to display data, mapping over the data, ternary, state and handler for opening folders and some styling

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,17 +1,49 @@
+import { useState } from 'react';
 import { data } from '../data';
 
 const Table = () => {
+	const [isVisible, setIsVisible] = useState<boolean>(false);
+
+	const handleFolders = () => {
+		setIsVisible((prevState) => !prevState);
+	};
+
 	return (
 		<section>
-			{data.map((document) => (
-				<table>
-					<tbody>
-						<tr>
-							<th>{document.name}</th>
-						</tr>
-					</tbody>
-				</table>
-			))}
+			<table className='mx-auto mt-10 text-left bg-slate-800 rounded'>
+				<tbody>
+					<tr>
+						<th className='py-2 px-3'>All documents</th>
+					</tr>
+					{data.map((document, key) =>
+						document.type === 'folder' ? (
+							<tr
+								key={key}
+								className='px-6 py-4 whitespace-nowrap border-b border-neutral-400 rounded hover:cursor-pointer'
+								onClick={handleFolders}
+							>
+								<td className='px-3 py-2 font-bold'>{document.name}</td>
+								<td className='px-3 py-2'>
+									{isVisible &&
+										document.files?.map((file, fileKey) => (
+											<span key={fileKey} className='px-4'>
+												{file.name}
+											</span>
+										))}
+								</td>
+							</tr>
+						) : (
+							<tr
+								key={key}
+								className='px-6 py-4 whitespace-nowrap border-b border-neutral-400 rounded'
+							>
+								<td className='px-3 py-2 font-bold'>{document.name}</td>
+								<td className='px-3 py-2'></td>
+							</tr>
+						)
+					)}
+				</tbody>
+			</table>
 		</section>
 	);
 };
